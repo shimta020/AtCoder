55e9655a2a99f904299472851f9ae4955ed246e8
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-22 22:34:04 +0900

    202:C(clear)

--- 202/c.rb
+++ 202/c.rb
@@ -1,3 +1,19 @@
+# N = gets.to_i
+# a_s = gets.split.map(&:to_i)
+# b_s = gets.split.map(&:to_i)
+# c_s = gets.split.map(&:to_i)
+
+# ans = 0
+
+# timeout
+# a_s.each do |a|
+#   N.times do |n|
+#     ans += 1 if a == b_s[c_s[n]-1]
+#   end
+# end
+
+# puts ans
+
 N = gets.to_i
 a_s = gets.split.map(&:to_i)
 b_s = gets.split.map(&:to_i)
@@ -5,10 +21,23 @@ c_s = gets.split.map(&:to_i)
 
 ans = 0
 
-a_s.each do |a|
-  N.times do |n|
-    ans += 1 if a == b_s[c_s[n]-1]
-  end
+b_c = []
+N.times do |n|
+  b_c << b_s[c_s[n]-1]
+end
+
+result_a = Array.new(N, 0)
+a_s.each do |n|
+  result_a[n-1] += 1
+end
+
+result_bc = Array.new(N, 0)
+b_c.each do |n|
+  result_bc[n-1] += 1
+end
+
+N.times do |n|
+  ans += result_a[n] * result_bc[n]
 end
 
 puts ans
