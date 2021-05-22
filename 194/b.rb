f443e980803327df315e540cf88518f02f21abfd
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-22 10:23:16 +0900

    194:B

--- 194/b.rb
+++ 194/b.rb
@@ -4,4 +4,13 @@ N.times do
   works << gets.split.map(&:to_i)
 end
 
-p works
+tmp_1 = (works.transpose[0]).sort
+tmp_2 = (works.transpose[1]).sort
+
+min_1 = tmp_1[1]
+min_2 = tmp_2[1]
+min_3 = tmp_1[0] + tmp_2[0]
+
+ans = [min_1, min_2, min_3].min
+
+puts ans
