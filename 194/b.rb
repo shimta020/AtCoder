257cf04de787373266871f77c3759646fac199ab
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-22 11:04:13 +0900

    196:B(fixed)

--- 194/b.rb
+++ 194/b.rb
@@ -4,13 +4,18 @@ N.times do
   works << gets.split.map(&:to_i)
 end
 
-tmp_1 = (works.transpose[0]).sort
-tmp_2 = (works.transpose[1]).sort
+tmp_1, tmp_2 = works.transpose
+ans = []
 
-min_1 = tmp_1[1]
-min_2 = tmp_2[1]
-min_3 = tmp_1[0] + tmp_2[0]
+tmp_1.each_with_index do |tmp1, i_1|
+  tmp_2.each_with_index do |tmp2, i_2|
+    if i_1 == i_2
+      time = tmp1 + tmp2
+    else
+      time = [tmp1, tmp2].max
+    end
+    ans << time
+  end
+end
 
-ans = [min_1, min_2, min_3].min
-
-puts ans
+puts ans.min
