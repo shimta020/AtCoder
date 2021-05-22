78f3ad3f4b89d41179641f7f524789b6799b960f
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-22 21:18:12 +0900

    202:B

--- 202/b.rb
+++ 202/b.rb
@@ -1,2 +1,14 @@
-s = gets.strip.split('')
-p s
+ints = gets.strip.split('').reverse
+
+ans =
+ints.map do |n|
+  if n == '6'
+    n = '9'
+  elsif n == '9'
+    n = '6'
+  else
+    n
+  end
+end
+
+puts ans.join
