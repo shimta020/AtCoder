eec32763e457a901129d81e2d6367175dffdc80f
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-05-31 22:43:28 +0900

    187:C(AC)

--- 187/c.rb
+++ 187/c.rb
@@ -1,18 +1,27 @@
+# n = gets.to_i
+# arr = []
+# n.times {arr << gets.strip}
+# ans = 'satisfiable'
+
+# arr.each do |a|
+#   if a.chars.first == '!'
+#     str = a.chars[1..(a.chars.size - 1)]
+#     ans = str if arr.include?(str)
+#     break
+#   else
+#     ans = a if arr.include?('!' + a)
+#     break
+#   end
+# end
+
+# puts ans
+
 n = gets.to_i
-arr = []
-n.times {arr << gets.strip}
-ans = 'satisfiable'
-
-arr.each do |a|
-  if a.chars.first == '!'
-    str = a[1..(a.size - 1)]
-    ans = str if arr.include?(str)
-    break
-  else
-    str = a
-    ans = str if arr.include?('!' + str)
-    break
-  end
-end
-
-puts ans
+ 
+arr = Array.new(n) { gets.chomp }.sort
+s1, s2 = arr.partition { |s| s.include?('!') }
+s1 = s1.map{|s| s.tr('!', '') }
+ 
+s = s1 & s2
+ 
+puts s.empty? ? 'satisfiable' : s.first
