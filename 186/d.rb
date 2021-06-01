ac899685b520e674f4a9e00cce1f74d32d49d9e1
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-01 18:08:05 +0900

    186:D(AC)

--- 186/d.rb
+++ 186/d.rb
@@ -2,10 +2,27 @@ n = gets.to_i
 arr = gets.split.map(&:to_i)
 
 ans = 0
-for x in 0..(n-1)
-  for y in x..(n-1)
-    ans += (arr[x] - arr[y]).abs
-  end
+
+# for x in 0..(n-1)
+#   for y in x..(n-1)
+#     ans += (arr[x] - arr[y]).abs
+#   end
+# end
+
+
+# arrを降順にすればabsメソッドをn**2回実行するのを減らせる
+# arr = arr.sort.reverse
+# for x in 0..(n-1)
+#   for y in x..(n-1)
+#     ans += arr[x] - arr[y]
+#   end
+# end
+
+
+arr = arr.sort
+
+for i in 1..(n-1)
+  ans += i*arr[i] - arr[0..i-1].inject(:+)
 end
 
 puts ans
