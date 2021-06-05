e086a7da8b9438401763651c85e957ae43a282fd
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-05 20:20:26 +0900

    182:B(別解)

--- 182/b.rb
+++ 182/b.rb
@@ -13,3 +13,9 @@ end
 divs = divs.group_by(&:itself).transform_values(&:size)
 
 p divs.max{|a, b| a[1] <=> b[1]}[0]
+
+# 美しそうな回答
+# n = gets.to_i
+# arr = gets.split.map(&:to_i)
+
+# puts (2 .. arr.max).max_by { |k| arr.count { |a| a % k == 0 } }
