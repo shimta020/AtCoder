0b4ea0360e9df9e1559966ed0deb706109c8db65
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-14 10:15:34 +0900

    179:C(AC)

--- 179/c.rb
+++ 179/c.rb
@@ -1,11 +1,22 @@
-require 'prime'
+# TLE
+# require 'prime'
+
+# n = gets.to_i
+
+# ans = 1
+
+# (2..(n-1)).each do |n|
+#   ans += Prime.prime_division(n).map {|p, e| e + 1 }.inject(:*)
+# end
+
+# puts ans
 
 n = gets.to_i
 
-ans = 1
+ans = 0
 
-(2..(n-1)).each do |n|
-  ans += Prime.prime_division(n).map {|p, e| e + 1 }.inject(:*)
+(1..n).each do |a|
+  ans += (n-1)/a
 end
 
 puts ans
