6a5e865b3a6fb858714394b1dd8688c0fedd9d13
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-19 21:44:04 +0900

    206/C(AC)

--- 206/c.rb
+++ 206/c.rb
@@ -1,4 +1,29 @@
+# require 'benchmark'
+# include Benchmark
+
+# n = gets.to_i
+# arr = gets.split.map(&:to_i)
+
+# def sample(arr)
+#   arr.combination(2).to_a.select{ |a,b| a != b}.size
+# end
+
+# Benchmark.benchmark(CAPTION, 7, FORMAT) do |x|
+#   x.report("sample:"){ sample(arr) }
+# end
+
 n = gets.to_i
 arr = gets.split.map(&:to_i)
 
-p arr.combination(2).to_a.select{ |a,b| a != b}.size
+total = (n*(n-1))/2
+rejects = 0
+
+arr = arr.group_by(&:itself).transform_values(&:size).sort
+
+arr.each do |a|
+  if a[1] != 1
+    rejects += (a[1]*(a[1]-1))/2
+  end
+end
+
+puts total - rejects
