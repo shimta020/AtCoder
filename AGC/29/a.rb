5d240d8e22161ac1a2f27a7eeeda07cb57154dce
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-25 18:43:31 +0900

    AGC:29:A(AC)

--- AGC/29/a.rb
+++ AGC/29/a.rb
@@ -1,14 +1,39 @@
+# s = gets.chomp.chars
+# ans = 0
+
+# TLE
+# while s.join.include?('BW')
+#   s.each_with_index do |val, idx|
+#     if val == 'B' && s[idx + 1] == 'W'
+#       s[idx] = 'W'
+#       s[idx + 1] = 'B'
+#       ans += 1
+#     end
+#   end
+# end
+
+# puts ans
+
+# これもTLE
+# s = gets.chomp
+# ans = 0
+
+# while s.include?('BW')
+#   ans += s.scan(/BW/).size
+#   s.gsub!('BW','WB')
+# end
+
+# puts ans
+
 s = gets.chomp.chars
 
 ans = 0
+w_count = 0
 
-while s.join.include?('BW')
-  s.each_with_index do |val, idx|
-    if val == 'B' && s[idx + 1] == 'W'
-      s[idx] = 'W'
-      s[idx + 1] = 'B'
-      ans += 1
-    end
+s.each.with_index(1) do |char, idx|
+  if char == 'W'
+    w_count += 1
+    ans += idx - w_count
   end
 end
 
