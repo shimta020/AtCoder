a94ca0ebf110aeaf628fe6e24698607184758322
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-06-26 13:02:25 +0900

    127:C(AC)

--- ABC/127/c.rb
+++ ABC/127/c.rb
@@ -2,12 +2,30 @@ n, m = gets.split.map(&:to_i)
 arr = []
 m.times{ arr << gets.split.map(&:to_i) }
 
-len = arr.size - 1
-tmp = (arr[0][0]..arr[0][1]).to_a
+# TLE
+# len = arr.size - 1
+# tmp = (arr[0][0]..arr[0][1]).to_a
 
-1.upto(len) do |i|
-  tmp &= (arr[i][0]..arr[i][1]).to_a
-  break if tmp.size == 0
+# 1.upto(len) do |i|
+#   tmp &= (arr[i][0]..arr[i][1]).to_a
+#   break if tmp.size == 0
+# end
+
+# puts tmp.size
+
+len = arr.size - 1
+head = 0
+tail = 10**5
+
+(0..len).each do |i|
+  # p head
+  # p tail
+  head = arr[i][0] if head <= arr[i][0]
+  tail = arr[i][1] if tail >= arr[i][1]
+  # p head
+  # p tail
+  # puts '-------------'
+  break if head == tail
 end
 
-puts tmp.size
+puts tail - head + 1
